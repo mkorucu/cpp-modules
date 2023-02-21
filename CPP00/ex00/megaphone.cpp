a105b6c1a3a46d11ff542bfc2f1bf2f029a408fe
mehmet soner korucu <32421915+mkorucu@users.noreply.github.com>
2023-02-21 15:11:02 +0300

    updated

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkorucu <mkorucu@student.42istanbul.com    +#+  +:+       +#+        */
+/*   By: mkorucu <mkorucu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:30:28 by mkorucu           #+#    #+#             */
-/*   Updated: 2023/02/21 09:51:02 by mkorucu          ###   ########.fr       */
+/*   Updated: 2023/02/21 14:51:29 by mkorucu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-using namespace std;
 
 char	ft_to_upper(char *str)
 {
@@ -26,13 +24,13 @@ int main(int ac, char **av)
 	int	i = 0, j;
 	
 	if (ac == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	while (ac > 1 && av[++i])
 	{
 		j = -1;
 		while (av[i][++j])
-			cout << ft_to_upper(&av[i][j]);
+			std::cout << ft_to_upper(&av[i][j]);
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }

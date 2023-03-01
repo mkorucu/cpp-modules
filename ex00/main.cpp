be0b33b2d708d6209dfea695d78550fac472bf19
mehmet soner korucu <32421915+mkorucu@users.noreply.github.com>
2023-03-01 15:34:33 +0300

    check

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,14 +7,15 @@ int	main()
 {
 	std::string	name;
 	Zombie		*_heapZombie;
-
+{
 	std::cout << "randomChump() calling" << std::endl;
 	randomChump("Random");
 	std::cout << "New Zombie arises from dead on the stack" << std::endl;
 	std::cout << "name of the Zombie is: ";
 	std::cin >> name;
 	Zombie	_stackZombie(name);
-
+	_stackZombie.announce();
+}
 	std::cout << "Another Zombie arises from dead on the heap" << std::endl;
 	std::cout << "name of the Zombie is: ";
 	std::cin >> name;
